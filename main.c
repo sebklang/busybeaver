51cd065546746e9175b00362adf7a22eb687d325
Sebastian Klang <sebklang@gmail.com>
2024-12-18 22:24:20 +0100

    asdf2

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+static unsigned long long int num_steps = 0;
+
 typedef struct tm_delta {
     unsigned char output;
     char dir; // L or R
@@ -11,7 +13,7 @@ typedef struct tm_delta {
 typedef struct tmch {
     char state;
     size_t strip_len; // BYTES
-    size_t counter; // BITS
+    long long int head; // BITS
     tm_delta *table;
     unsigned char *strip;
 } tmch;
@@ -19,8 +21,8 @@ typedef struct tmch {
 int tmch_step(tmch *tm)
 {
     int state_idx = tm->state - 'A';
-    size_t byte_idx = tm->counter / 8;
-    size_t bit_offset = tm->counter % 8;
+    size_t byte_idx = tm->head / 8;
+    size_t bit_offset = tm->head % 8;
     unsigned char byte = tm->strip[byte_idx];
     unsigned char input = (byte >> bit_offset) & 0x01;
     int step_index = 2 * state_idx + input;
@@ -29,8 +31,20 @@ int tmch_step(tmch *tm)
     unsigned char new_byte = byte | output_mask;
     tm->strip[byte_idx] = new_byte;
     switch (step->dir) {
-    case 'L': tm->counter--; break;
-    case 'R': tm->counter++; break;
+    case 'L':
+        if (tm->head <= 0) {
+            fprintf(stderr, "Head ran off the left after %lld steps.\n", num_steps);
+            exit(EXIT_FAILURE);
+        }
+        tm->head--;
+        break;
+    case 'R':
+        if (tm->head >= tm->strip_len * 8 - 1) {
+            fprintf(stderr, "Head ran off the right after %lld steps.\n", num_steps);
+            exit(EXIT_FAILURE);
+        }
+        tm->head++;
+        break;
     default:
         fprintf(stderr, "default in tmch_step");
         return -1;
@@ -49,15 +63,22 @@ int main(int argc, char **argv)
         {1, 'R', 'Z'}, {0, 'L', 'A'},
     };
 
-    tmch my_tm = {'A', 1 << 13, (1 << 12) * 8, my_table};
-    my_tm.strip = calloc(my_tm.strip_len, sizeof(unsigned char));
+    tm_delta my_table_sus[] = {
+        {1, 'L', 'B'}, {1, 'R', 'C'},
+        {1, 'L', 'C'}, {1, 'L', 'B'},
+        {1, 'L', 'D'}, {0, 'R', 'E'},
+        {1, 'R', 'A'}, {1, 'R', 'D'},
+        {1, 'L', 'Z'}, {0, 'R', 'A'},
+    };
 
-    unsigned long long int counter = 0ull;
+    tmch my_tm = {'A', 1 << 14, (1 << 13) * 8, my_table};
+    my_tm.strip = calloc(my_tm.strip_len, sizeof(unsigned char));
+    printf("SUS %ld\n", my_tm.strip_len);
 
     while (my_tm.state != 'Z') {
-        printf("%c", my_tm.state);
+//        printf("%c", my_tm.state);
         tmch_step(&my_tm);
-        counter++;
+        num_steps++;
     }
     printf("%c\n", my_tm.state);
 
@@ -68,7 +89,7 @@ int main(int argc, char **argv)
         }
     }
 
-    printf("Turing machine terminated after %d steps.\n", counter);
+    printf("Turing machine terminated after %lld steps.\n", num_steps);
 
     return 0;
 }
