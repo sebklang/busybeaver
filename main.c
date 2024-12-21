4d3bd2f3d799cc7e6a47a883e507864ad8a22ac0
Sebastian Klang <sebklang@gmail.com>
2024-12-21 12:38:45 +0100

    rename

--- main.c
+++ main.c
@@ -17,10 +17,10 @@ typedef struct tm_delta {
 
 typedef struct tmch {
     char state;
-    size_t strip_len; // BYTES
+    size_t tape_len; // BYTES
     long long int head; // BITS
     tm_delta *table;
-    unsigned char *strip;
+    unsigned char *tape;
 } tmch;
 
 void tmch_step(tmch *tm)
@@ -28,13 +28,13 @@ void tmch_step(tmch *tm)
     int state_index = tm->state - 'A';
     size_t byte_index = tm->head / 8;
     size_t bit_offset = tm->head % 8;
-    unsigned char byte = tm->strip[byte_index];
+    unsigned char byte = tm->tape[byte_index];
     unsigned char input = (byte >> bit_offset) & 0x01;
     int delta_index = 2 * state_index + input;
     tm_delta *delta = &tm->table[delta_index];
     unsigned char output_mask = delta->output << bit_offset;
     unsigned char new_byte = byte | output_mask;
-    tm->strip[byte_index] = new_byte;
+    tm->tape[byte_index] = new_byte;
     switch (delta->dir) {
     case 'L': tm->head--; break;
     case 'R': tm->head++; break;
@@ -62,8 +62,8 @@ int main(int argc, char **argv)
 
     // Initialize turing machine
     tmch tm = {'A', 1 << 12, (1 << 11) * 8, my_table};
-    tm.strip = calloc(tm.strip_len, sizeof(unsigned char));
-    printf("Started turing machine with %ld bytes\n", tm.strip_len);
+    tm.tape = calloc(tm.tape_len, sizeof(unsigned char));
+    printf("Started turing machine with %ld bytes\n", tm.tape_len);
 
     // Emulate turing machine
     while (true) {
@@ -78,17 +78,17 @@ int main(int argc, char **argv)
         if (tm.head < 0) {
             stopping_reason = REACHED_LEFT_EDGE; break;
         }
-        if (tm.head >= tm.strip_len * 8) {
+        if (tm.head >= tm.tape_len * 8) {
             stopping_reason = REACHED_RIGHT_EDGE; break;
         }
     }
 
-    // Print end strip contents
-    for (int i = 0; i < tm.strip_len; i++) {
+    // Print end tape contents
+    for (int i = 0; i < tm.tape_len; i++) {
         if (i % 16 == 0) {
             printf("\n%04X    ", i);
         }
-        printf("%02X ", tm.strip[i]);
+        printf("%02X ", tm.tape[i]);
     }
     printf("\n");
 
