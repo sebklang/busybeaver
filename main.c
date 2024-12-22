a274a8bb070eda7bb7f48179f8c4567e23057284
Sebastian Klang <sebklang@gmail.com>
2024-12-22 19:42:03 +0100

    im stuff (reversing bug)

--- main.c
+++ main.c
@@ -30,8 +30,8 @@ void tmch_step(tmch *tm)
     size_t bit_offset = tm->head % 8;
     unsigned char byte = tm->tape[byte_index];
     unsigned char input = (byte >> bit_offset) & 0x01;
-    int delta_index = 2 * state_index + input;
-    tm_delta *delta = &tm->table[delta_index];
+    int table_index = 2 * state_index + input;
+    tm_delta *delta = &tm->table[table_index];
     unsigned char output_mask = delta->output << bit_offset;
     unsigned char new_byte = byte | output_mask;
     tm->tape[byte_index] = new_byte;
@@ -51,14 +51,19 @@ void tm_print(tmch *tm) {
             printf("\n%04X    ", i);
         }
         if (tm->head / 8 == i) {
-            printf("(%d)", tm->head % 8);
+            printf("(%lld)", tm->head % 8);
         }
-        printf("%02X ", tm->tape[i]);
+        unsigned char reversed = 0;
+        for (int i = 0; i < 8; i++) {
+            unsigned char ith_bit = (tm->tape[i] >> i) & 0x01;
+            reversed |= ith_bit << (7 - i);
+        }
+        printf("%02X ", reversed);
     }
     printf("\n");
 }
 
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
     unsigned long long int num_steps = 0ULL;
     const unsigned long long int max_steps = ~0 & ~(1ULL << 63);
@@ -76,12 +81,14 @@ int main(int argc, char **argv)
     // Initialize turing machine
     tmch tm = {'A', 1 << 10, (1 << 9) * 8, my_table};
     tm.tape = calloc(tm.tape_len, sizeof(unsigned char));
-    printf("Started turing machine with %ld bytes\n", tm.tape_len);
+    printf("Started TM with %ld bytes.\n", tm.tape_len);
 
     // Emulate turing machine
+    char c = '\0'; // Terminal input
+    printf("The bits are least-significant first.\n"
+           "Press enter to step, r to run, or q to quit.\n");
     while (true) {
-        tmch_step(&tm);
-        num_steps++;
+        // Stopping cases
         if (tm.state == 'Z') {
             stopping_reason = HALTED; break;
         }
@@ -94,15 +101,25 @@ int main(int argc, char **argv)
         if (tm.head >= tm.tape_len * 8) {
             stopping_reason = REACHED_RIGHT_EDGE; break;
         }
-        if (getchar() == 'q') {
-            exit(EXIT_SUCCESS);
+        // Handle input and execute TM step
+        if (c != 'r') {
+            c = getchar();
+            if (c == 'q') exit(EXIT_SUCCESS);
+            tmch_step(&tm);
+            num_steps++;
+            tm_print(&tm);
+            fflush(stdout);
         }
-        tm_print(&tm);
-        fflush(stdout);
+        else {
+            tmch_step(&tm);
+            num_steps++;
+        }
     }
 
     // Print end tape contents
-    tm_print(&tm);
+    if (c == 'r') {
+        tm_print(&tm);
+    }
 
     // Print halting reason
     switch (stopping_reason) {
